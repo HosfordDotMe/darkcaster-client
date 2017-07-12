61346a46fa076ba560542cf25038fe9bd0a8cd48
Eric Hosford <eric.hosford@gmail.com>
2017-07-12 18:58:32 -0400

    added image lookup

--- src/components/current-weather/current-weather.controller.js
+++ src/components/current-weather/current-weather.controller.js
@@ -1,7 +1,11 @@
 MainController.$inject = ['WeatherService'];
+const clearDay = require('../../images/clear-day.png');
 
 function MainController(weather){
     this.message = 'hello from angular';
+    this.imageLookup = {
+        'clear-day': clearDay
+    };
     this.weatherData = weather.getCurrentWeather();
 };
 
