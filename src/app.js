11ca8175c1f80b959298e1e24f20c45a463bf68d
Eric Hosford <eric.hosford@gmail.com>
2017-07-11 21:00:02 -0400

    migrated to component architecture

--- src/app.js
+++ src/app.js
@@ -3,16 +3,13 @@ const angular = require('angular');
 //services
 const WeatherService = require('./services/weather.service');
 
+//components
+const currentWeatherComponent = require('./components/current-weather/index');
+
+
 //create our application
 angular.module('darkcaster-client',[]); //setter syntax
 
 angular.module('darkcaster-client') //getter angular's version of require
-       .controller('MainController', MainController)
-       .factory('WeatherService', WeatherService);
-
-MainController.$inject = ['WeatherService'];
-
-function MainController(weather){
-    this.message = 'hello from angular';
-    this.weatherData = weather.getCurrentWeather();
-};
+       .factory('WeatherService', WeatherService)
+       .component('currentWeather', currentWeatherComponent);
