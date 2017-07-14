08f9cab7a2ec9b723e1cc6259c5840b501d824c5
Eric Hosford <eric.hosford@gmail.com>
2017-07-14 19:42:55 -0400

    pulled image lookup into  its own service to setup for future components

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const angular = require('angular');
 
 //services
 const WeatherService = require('./services/weather.service');
+const ImagesService = require('./services/images.service');
 
 //components
 const currentWeatherComponent = require('./components/current-weather/');
@@ -10,4 +11,5 @@ const currentWeatherComponent = require('./components/current-weather/');
 //create our application
 angular.module('darkcaster-client',[]) //setter syntax
        .factory('WeatherService', WeatherService)
+       .factory('ImagesService', ImagesService)
        .component('currentWeather', currentWeatherComponent);
--- src/components/current-weather/current-weather.controller.js
+++ src/components/current-weather/current-weather.controller.js
@@ -1,28 +1,9 @@
-CurrentWeatherController.$inject = ['WeatherService'];
-const clearDay = require('../../images/clear-day.svg');
-const clearNight = require('../../images/clear-night.svg');
-const cloudy = require('../../images/cloudy.svg');
-const fog = require('../../images/fog.svg');
-const partlyCloudyDay = require('../../images/partly-cloudy-day.svg');
-const partlyCloudyNight = require('../../images/partly-cloudy-night.svg');
-const rain = require('../../images/rain.svg');
-const sleet = require('../../images/sleet.svg');
-const snow = require('../../images/snow.svg');
+CurrentWeatherController.$inject = ['WeatherService', 'ImagesService'];
 
-function CurrentWeatherController(weather){
+function CurrentWeatherController(weather, images){
     this.lat = 0;
     this.lon = 0;
-    this.imageLookup = {
-        'clear-day': clearDay,
-        'clear-night': clearNight,
-        'cloudy': cloudy,
-        'fog': fog,
-        'partly-cloudy-day': partlyCloudyDay,
-        'partly-cloudy-night': partlyCloudyNight,
-        'rain': rain,
-        'sleet': sleet,
-        'snow': snow
-    };
+    this.imageLookup = images.lookup;
     this.search = function search(){
         weather.getCurrentWeather(this.lat,this.lon)
                .then(currentWeather => this.weatherData = currentWeather);
